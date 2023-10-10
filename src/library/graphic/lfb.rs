2b555d584c3bba92fa57f63db05a744b400c98d7
Fabian Ruhland <ruhland@hhu.de>
2023-10-10 20:32:27 +0200

    lfb_terminal: Implement ANSI sequence parsing

--- src/library/graphic/lfb.rs
+++ src/library/graphic/lfb.rs
@@ -11,7 +11,7 @@ pub struct LFB {
     pixel_drawer: PixelDrawer
 }
 
-pub const CHAR_HEIGHT: u32 = 8;
+pub const CHAR_HEIGHT: u32 = 16;
 pub const CHAR_WIDTH: u32 = 8;
 
 impl LFB {
@@ -61,7 +61,7 @@ impl LFB {
         self.bpp
     }
 
-    pub fn draw_pixel(&self, x: u32, y: u32, color: &Color) {
+    pub fn draw_pixel(&self, x: u32, y: u32, color: Color) {
         // Check if pixel is outside the framebuffer
         if x >= self.width || y >= self.height {
             return;
@@ -70,7 +70,18 @@ impl LFB {
         unsafe { (self.pixel_drawer)(self.buffer, self.pitch, x, y, color) };
     }
 
-    pub fn draw_char(&self, x: u32, y: u32, fg_color: &Color, bg_color: &Color, c: char) -> bool {
+    pub fn fill_rect(&self, x: u32, y: u32, width: u32, height: u32, color: Color) {
+        let end_x = x + width;
+        let end_y = y + height;
+
+        for i in y..end_y {
+            for j in x..end_x {
+                self.draw_pixel(j, i, color);
+            }
+        }
+    }
+
+    pub fn draw_char(&self, x: u32, y: u32, fg_color: Color, bg_color: Color, c: char) -> bool {
         let mut glyph = BASIC_FONTS.get(c);
         if glyph.is_none() {
             glyph = LATIN_FONTS.get(c);
@@ -105,12 +116,13 @@ impl LFB {
                         _ => fg_color
                     };
 
-                    self.draw_pixel(x + x_offset, y + y_offset, &color);
+                    self.draw_pixel(x + x_offset, y + y_offset, color);
+                    self.draw_pixel(x + x_offset, y + y_offset + 1, color);
                     x_offset += 1;
                 }
 
                 x_offset = 0;
-                y_offset += 1;
+                y_offset += 2;
             }
 
             return true;
@@ -133,28 +145,28 @@ impl LFB {
     }
 }
 
-type PixelDrawer = unsafe fn(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color);
+type PixelDrawer = unsafe fn(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color);
 
-fn draw_pixel_stub(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color) {
+fn draw_pixel_stub(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color) {
     #![allow(unused_variables)]
     panic!("Using empty LFB!");
 }
 
-unsafe fn draw_pixel_15_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color) {
+unsafe fn draw_pixel_15_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color) {
     let index = (x + y * (pitch / 2)) as isize;
     let rgb = color.rgb_15();
 
     (addr as *mut u16).offset(index).write(rgb as u16) ;
 }
 
-unsafe fn draw_pixel_16_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color) {
+unsafe fn draw_pixel_16_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color) {
     let index = (x + y * (pitch / 2)) as isize;
     let rgb = color.rgb_16();
 
     (addr as *mut u16).offset(index).write(rgb as u16);
 }
 
-unsafe fn draw_pixel_24_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color) {
+unsafe fn draw_pixel_24_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color) {
     let index = (x * 3 + y * pitch) as isize;
     let rgb = color.rgb_24();
 
@@ -163,7 +175,7 @@ unsafe fn draw_pixel_24_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &C
     addr.offset(index + 2).write(((rgb >> 16) & 0xff) as u8);
 }
 
-unsafe fn draw_pixel_32_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: &Color) {
+unsafe fn draw_pixel_32_bit(addr: *mut u8, pitch: u32, x: u32, y: u32, color: Color) {
     let index = (x + y * (pitch / 4)) as isize;
     let rgb = color.rgb_32();
 
