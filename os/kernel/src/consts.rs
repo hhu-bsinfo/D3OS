2979f9bcfd2a15550860e0586a87787d8c472b1f
schoettner <michael.schoettner@hhu.de>
2024-06-17 14:51:35 +0200

    consts.rs, thread.rs: update comments

--- os/kernel/src/consts.rs
+++ os/kernel/src/consts.rs
@@ -1,5 +1,6 @@
 
 
-pub const MAIN_USER_STACK_START: usize = 0x400000000000;
-pub const MAX_USER_STACK_SIZE: usize = 0x40000000;
+pub const MAIN_USER_STACK_START: usize = 0x400000000000;  // 10 TiB
+pub const MAX_USER_STACK_SIZE: usize = 0x40000000;  // 1 GiB
 pub const KERNEL_STACK_PAGES: usize = 64;
+pub const STACK_ENTRY_SIZE: usize = 8;  
