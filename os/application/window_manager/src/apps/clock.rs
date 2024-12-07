065102684002cd888c8134d6789c33617962ca11
Tiemo Timtschenko <tiemo.timtschenko@gmx.de>
2024-12-07 23:31:38 +0100

    signal variable duplicate clock example

--- os/application/window_manager/src/apps/clock.rs
+++ os/application/window_manager/src/apps/clock.rs
@@ -1,9 +1,8 @@
-use alloc::{boxed::Box, rc::Rc, string::ToString, vec};
+use alloc::{boxed::Box, rc::Rc, string::ToString};
 use drawer::vertex::Vertex;
-use spin::rwlock::RwLock;
 use time::date;
 
-use crate::{api::Command, WindowManager};
+use crate::{api::Command, signal::Signal, WindowManager};
 
 use super::runnable::Runnable;
 
@@ -14,25 +13,39 @@ impl Runnable for Clock {
         let handle = concurrent::thread::current().id();
         let api = WindowManager::get_api();
         let date_val = date().format("%Y-%m-%d %H:%M:%S").to_string();
-        let text_rc = Rc::new(RwLock::new(date_val));
-        let on_create_rc = Rc::clone(&text_rc);
         
-        let _ = api.execute(
+        let clock = Signal::new(date_val);
+        let second_clock = Rc::clone(&clock);
+        
+        let _clock = api.execute(
             handle,
             Command::CreateLabel {
                 log_pos: Vertex::new(200, 200),
-                text: text_rc,
+                text: Rc::clone(&clock),
                 on_loop_iter: Some(Box::new(move || {
-                    let mut date_val = on_create_rc.write();
+                    let old_date = clock.get();
                     let new_date = date().format("%Y-%m-%d %H:%M:%S").to_string();
-                    if *date_val != new_date {
-                        *date_val = new_date;
+
+                    if old_date != new_date {
+                        clock.set(new_date);
                         return true;
                     }
+
                     return false;
                 })),
                 font_size: Some(4),
-                on_change_redraw: vec![],
+                styling: None,
+            },
+        );
+
+        let _second_clock = api.execute(
+            handle,
+            Command::CreateLabel {
+                log_pos: Vertex::new(200, 300),
+                text: Rc::clone(&second_clock),
+                on_loop_iter: None,
+                font_size: Some(4),
+                styling: None,
             },
         );
     }
