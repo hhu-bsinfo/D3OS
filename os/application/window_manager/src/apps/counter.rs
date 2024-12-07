9d9dc81ffa194f5bf57e2a9e00d39742c4f4db98
Tiemo Timtschenko <tiemo.timtschenko@gmx.de>
2024-12-07 23:42:08 +0100

    counter with disabling checkbox example

--- os/application/window_manager/src/apps/counter.rs
+++ os/application/window_manager/src/apps/counter.rs
@@ -1,7 +1,7 @@
-use crate::alloc::string::ToString;
-use alloc::{boxed::Box, rc::Rc, string::String, vec};
+use crate::{alloc::string::ToString, signal::{ComponentRef, Signal}};
+use alloc::{boxed::Box, rc::Rc, string::String};
 use drawer::{rect_data::RectData, vertex::Vertex};
-use spin::mutex::Mutex;
+use spin::RwLock;
 
 use crate::{api::Command, WindowManager};
 
@@ -14,43 +14,81 @@ impl Runnable for Counter {
         let handle = concurrent::thread::current().id();
         let api = WindowManager::get_api();
 
-        let label_text_rc1 = Rc::new(Mutex::new(String::from("0")));
-        let label_text_rc2 = Rc::clone(&label_text_rc1);
-        let label_text_rc3 = Rc::clone(&label_text_rc1);
-        
-        let counter_button = api.execute(
-            handle,
-            Command::CreateButton {
-                log_rect_data: RectData {
-                    top_left: Vertex::new(200, 200),
-                    width: 200,
-                    height: 50,
-                },
-                label: Some((label_text_rc1, 1)),
-                on_click: Box::new(move || {
-                    let mut value = label_text_rc2.lock();
-                    let old = (*value).parse::<usize>().unwrap();
-                    *value = (old + 1).to_string();
-                }),
-                on_change_redraw: vec![],
-            },
-        ).unwrap();
+        let label = Signal::new(String::from("0"));
+        let label_inc = Rc::clone(&label);
+        let label_reset = Rc::clone(&label);
 
-        let _ = api.execute(
+        let counter_button: Rc<RwLock<Option<ComponentRef>>> = Rc::new(RwLock::new(None));
+        let reset_button: Rc<RwLock<Option<ComponentRef>>> = Rc::new(RwLock::new(None));
+
+        let counter_button_checkbox = Rc::clone(&counter_button);
+        let reset_button_checkbox = Rc::clone(&reset_button);
+
+        api.execute(
             handle,
-            Command::CreateButton {
+            Command::CreateCheckbox {
                 log_rect_data: RectData {
-                    top_left: Vertex::new(200, 350),
-                    width: 200,
+                    top_left: Vertex::new(200, 50),
+                    width: 50,
                     height: 50,
                 },
-                label: Some((Rc::new(Mutex::new(String::from("Reset"))), 1)),
-                on_click: Box::new(move || {
-                    let mut value = label_text_rc3.lock();
-                    *value = String::from("0");
-                }),
-                on_change_redraw: vec![Rc::clone(&counter_button)],
+                state: true,
+                on_change: Some(Box::new(move |checked: bool| {
+                    if let Some(counter_button) = counter_button_checkbox.read().as_ref() {
+                        if let Some(disableable) = counter_button.write().as_disableable_mut() {
+                            if checked {
+                                disableable.enable();
+                            } else {
+                                disableable.disable();
+                            }
+                        }
+                    }
+
+                    if let Some(reset_button) = reset_button_checkbox.read().as_ref() {
+                        if let Some(disableable) = reset_button.write().as_disableable_mut() {
+                            if checked {
+                                disableable.enable();
+                            } else {
+                                disableable.disable();
+                            }                        }
+                    }
+                })),
+                styling: None,
             },
         );
+
+
+        *counter_button.write() = Some(api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(200, 150),
+                    width: 200,
+                    height: 50,
+                },
+                label: Some((Rc::clone(&label), 1)),
+                on_click: Some(Box::new(move || {
+                    let old = (label_inc.get()).parse::<usize>().unwrap();
+                    label_inc.set((old + 1).to_string());
+                })),
+                styling: None,
+            },
+        ).unwrap());
+
+        *reset_button.write() = Some(api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(200, 250),
+                    width: 200,
+                    height: 50,
+                },
+                label: Some((Signal::new(String::from("Reset")), 1)),
+                on_click: Some(Box::new(move || {
+                    label_reset.set(String::from("0"));
+                })),
+                styling: None,
+            },
+        ).unwrap());
     }
 }
