5d5aaf50f935a04ba6c8f8807fee2605d2afb87b
Tiemo Timtschenko <tiemo.timtschenko@gmx.de>
2024-12-07 23:42:36 +0100

    calculator example app for many redrawing operations

---
+++ os/application/window_manager/src/apps/calculator.rs
@@ -0,0 +1,260 @@
+use crate::{alloc::string::ToString, components::component::ComponentStylingBuilder, signal::{ComponentRef, Signal}};
+use alloc::{boxed::Box, format, rc::Rc, string::String, vec};
+use drawer::{rect_data::RectData, vertex::Vertex};
+use graphic::color::{Color, GREY, WHITE};
+use spin::rwlock::RwLock;
+
+use crate::{api::Command, WindowManager};
+
+use super::runnable::Runnable;
+
+pub struct Calculator;
+
+impl Runnable for Calculator {
+    fn run() {
+        let handle = concurrent::thread::current().id();
+        let api = WindowManager::get_api();
+
+        let display_text = Signal::new(String::from("0"));
+        let equals_button: Rc<RwLock<Option<ComponentRef>>> = Rc::new(RwLock::new(None));
+
+        // Display Label
+        api.execute(
+            handle,
+            Command::CreateLabel {
+                log_pos: Vertex::new(50, 50),
+                font_size: Some(1),
+                text: Rc::clone(&display_text),
+                on_loop_iter: None,
+                styling: None,
+            },
+        );
+
+        // Helper to create number buttons
+        let create_number_button = |x: u32, y: u32, number: usize| {
+            let text_clone = Rc::clone(&display_text);
+
+            api.execute(
+                handle,
+                Command::CreateButton {
+                    log_rect_data: RectData {
+                        top_left: Vertex::new(x, y),
+                        width: 50,
+                        height: 50,
+                    },
+                    label: Some((Signal::new(number.to_string()), 1)),
+                    on_click: Some(Box::new(move || {
+                        let value = text_clone.get();
+                        if value == "0" {
+                            text_clone.set(number.to_string());
+                        } else {
+                            text_clone.set(format!("{}{}", value, number));
+                        }
+                    })),
+                    styling: Some(
+                        ComponentStylingBuilder::new()
+                        .background_color(GREY.bright())
+                        .focused_background_color(GREY.bright())
+                        .text_color(WHITE.bright())
+                        .focused_text_color(WHITE.bright())
+                        .build()
+                    ),
+                },
+            );
+        };
+
+        // Number Buttons
+        for i in 0..10 {
+            let mut x = 50 + (i % 3) * 60;
+            let y = 120 + (i / 3) * 60;
+
+            create_number_button(x, y, i as usize);
+        }
+
+        let display_decimal = Rc::clone(&display_text);
+
+        api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(110, 300),
+                    width: 50,
+                    height: 50,
+                },
+                label: Some((Signal::new(String::from(".")), 1)),
+                on_click: Some(Box::new(move || {
+                    let value = display_decimal.get();
+                    if !value.contains('.') {
+                        display_decimal.set(format!("{}.", value));
+                    }
+                })),
+                styling: None,
+            }
+        );
+
+        let operators = vec!["+", "-", "*", "/"];
+        let current_operator = Rc::new(RwLock::<Option<String>>::new(None));
+        let stored_value: Rc<RwLock<f64>> = Rc::new(RwLock::new(0.0));
+
+        let display_sign = Rc::clone(&display_text);
+
+        api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(170, 300),
+                    width: 50,
+                    height: 50,
+                },
+                label: Some((Signal::new(String::from("+/-")), 1)),
+                on_click: Some(Box::new(move || {
+                    let mut value = display_sign.get().parse::<f64>().unwrap_or(0.0);
+                    value *= -1.0;
+                    display_sign.set(value.to_string());
+                })),
+                styling: None,
+            }
+        );
+
+        // Equals Button
+        let equals_clone = Rc::clone(&display_text);
+        let equals_operator_clone = Rc::clone(&current_operator);
+        let equals_stored_clone = Rc::clone(&stored_value);
+        
+        // Operator Buttons
+        for (i, op) in operators.iter().enumerate() {
+            let op = op.to_string();
+            let text_clone = Rc::clone(&display_text);
+            let operator_clone = Rc::clone(&current_operator);
+            let stored_value_clone = Rc::clone(&stored_value);
+
+            let equals_button_in_operator = Rc::clone(&equals_button);
+
+            api.execute(
+                handle,
+                Command::CreateButton {
+                    log_rect_data: RectData {
+                        top_left: Vertex::new(230, 120 + (i as u32 * 60)),
+                        width: 50,
+                        height: 50,
+                    },
+                    label: Some((Signal::new(op.clone()), 1)),
+                    on_click: Some(Box::new(move || {
+                        let value = text_clone.get();
+                        let mut stored = stored_value_clone.write();
+                        *stored = value.parse::<f64>().unwrap_or(0.0);
+
+                        text_clone.set(String::from("0"));
+
+                        let mut operator = operator_clone.write();
+                        *operator = Some(op.clone());
+
+                        if let Some(equals_button) = equals_button_in_operator.read().as_ref() {
+                            if let Some(disableable) = equals_button.write().as_disableable_mut() {
+                                disableable.enable();
+                            }
+                        }
+                    })),
+                    styling: None,
+                },
+            );
+        }
+
+        let display_clear = Rc::clone(&display_text);
+
+        let equals_button_in_clear = Rc::clone(&equals_button);
+
+        let stored_value_clear = Rc::clone(&stored_value);
+
+        const CLEAR_BUTTON_COLOR: Color = Color {
+            red: 255,
+            green: 165,
+            blue: 0,
+            alpha: 255,
+        };
+
+        // Clear Button
+        api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(50, 360),
+                    width: 110,
+                    height: 50,
+                },
+                label: Some((Signal::new(String::from("C")), 1)),
+                on_click: Some(Box::new(move || {
+                    display_clear.set(String::from("0"));
+
+                    let mut stored = stored_value_clear.write();
+                    *stored = 0.0;
+
+                    let mut operator = current_operator.write();
+                    *operator = None;
+
+                    if let Some(equals_button) = equals_button_in_clear.read().as_ref() {
+                        if let Some(disableable) = equals_button.write().as_disableable_mut() {
+                            disableable.disable();
+                        }
+                    }
+                })),
+                styling: Some(
+                    ComponentStylingBuilder::new()
+                    .text_color(WHITE.bright())
+                    .background_color(CLEAR_BUTTON_COLOR)
+                    .disabled_background_color(CLEAR_BUTTON_COLOR)
+                    .focused_background_color(CLEAR_BUTTON_COLOR)
+                    .selected_background_color(CLEAR_BUTTON_COLOR)
+                    .border_color(CLEAR_BUTTON_COLOR)
+                    .disabled_border_color(CLEAR_BUTTON_COLOR)
+                    .build()
+                ),
+            },
+        );
+
+        let equals_button_init = Rc::clone(&equals_button);
+        *equals_button_init.write() = Some(api.execute(
+            handle,
+            Command::CreateButton {
+                log_rect_data: RectData {
+                    top_left: Vertex::new(170, 360),
+                    width: 110,
+                    height: 50,
+                },
+                label: Some((Signal::new(String::from("=")), 1)),
+                on_click: Some(Box::new(move || {
+                    let value = equals_clone.get();
+                    let stored = *equals_stored_clone.write();
+                    let operator: Option<String> = equals_operator_clone.read().clone();
+
+                    if let Some(op) = operator {
+                        let current_value = value.parse::<f64>().unwrap_or(0.0);
+                        let result = match op.as_str() {
+                            "+" => stored + current_value,
+                            "-" => stored - current_value,
+                            "*" => stored * current_value,
+                            "/" => {
+                                if current_value != 0.0 {
+                                    stored / current_value
+                                } else {
+                                    return; // Division durch 0 verhindern
+                                }
+                            }
+                            _ => return,
+                        };
+
+                        equals_clone.set(result.to_string());
+                    }
+                })),
+                styling: None,
+            },
+        ).unwrap());
+
+        let equals_button_disabling = Rc::clone(&equals_button);
+        if let Some(equals_button) = equals_button_disabling.read().as_ref() {
+            if let Some(disableable) = equals_button.write().as_disableable_mut() {
+                disableable.disable();
+            }
+        };
+    }
+}
