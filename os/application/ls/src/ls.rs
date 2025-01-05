e4e8db6fea64b48026165dbc6b150c1f510edc3a
schoettner <michael.schoettner@hhu.de>
2025-01-05 14:17:56 +0100

    Shell supports now builtin commands: pwd, mkdir, cd, touch; A test app 'ntest' for the naming service; The 'ls' app

---
+++ os/application/ls/src/ls.rs
@@ -0,0 +1,81 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use naming::{mkdir, touch, cwd, cd};
+
+use naming::shared_types::{DirEntry, FileType, OpenOptions, SeekOrigin};
+#[allow(unused_imports)]
+use runtime::*;
+use terminal::{print, println};
+
+fn print_usage() {
+    println!("usage: ls [directory_name");
+}
+
+fn print_dir_entry(dentry: DirEntry) {
+    if dentry.file_type == FileType::Directory {
+        println!("d {}", dentry.name);
+    } else {
+        println!("- {}", dentry.name);
+    }
+}
+
+fn process_ls(path: &String) {
+    // open directory
+    let res = naming::open(&path, OpenOptions::DIRECTORY);
+    if res.is_err() {
+        print_usage();
+        return;
+    }
+    let fd = res.unwrap();
+
+    // dump content of directory
+    loop {
+        let res = naming::readdir(fd);
+        match res {
+            Ok(data) => {
+                match data {
+                    Some(content) => print_dir_entry(content),
+                    None => break,
+                }
+            },
+            Err(e) => { 
+                break;
+            },
+        }
+    }
+
+    // close directory
+    naming::close(fd);
+}
+
+pub fn args_to_vec() -> Vec<String> {
+    let args = env::args();
+    let mut vec = Vec::new();
+    for arg in args {
+        vec.push(arg);
+    }
+    vec
+}
+
+#[unsafe(no_mangle)]
+pub fn main() {
+    let args_vec = args_to_vec();
+    let args_count = args_vec.len();
+
+
+    if args_count == 1 {
+        let res = cwd();
+        match res {
+            Ok(path) =>  process_ls(&path),
+            Err(e) => print_usage(),
+        }
+    } else if args_count == 2 {
+        process_ls(&args_vec[1]);
+    } else {
+        print_usage();
+    }
+}
