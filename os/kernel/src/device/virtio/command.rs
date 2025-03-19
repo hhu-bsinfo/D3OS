677545ec154ca25bce1a54122d2d029addd0a172
nut97rif <elrich.nikitaaa@gmail.com>
2025-03-19 19:47:57 +0100

    Refactor GPU command structures and update comments for clarity

--- os/kernel/src/device/virtio/command.rs
+++ os/kernel/src/device/virtio/command.rs
@@ -1,16 +1,16 @@
-// device/virtio_gpu/command.rs
+// GPU Commands
+
 #[repr(C)]
 pub struct VirtioGpuCtrlHdr {
-    // Header-Felder laut Spec
+    // Command header fields
 }
 
 #[repr(C)]
 pub struct VirtioGpuResourceCreate2d {
-
+    // Fields for resource creation
 }
 
 #[repr(C)]
 pub struct VirtioGpuResourceFlush {
-
+    // Fields for resource flush
 }
-
