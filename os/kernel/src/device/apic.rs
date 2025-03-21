1efc2e11a0d8c6801cfeae7d1767dec33391b282
Fabian Ruhland <ruhland@hhu.de>
2025-03-21 11:28:20 +0100

    Update dependencies

--- os/kernel/src/device/apic.rs
+++ os/kernel/src/device/apic.rs
@@ -60,7 +60,7 @@ impl Apic {
 
         // Find APIC relevant structures in ACPI tables
         let madt_mapping = acpi_tables().lock().find_table::<Madt>().expect("MADT not available");
-			  let madt = madt_mapping.get();
+		let madt = madt_mapping.get();
         let int_model = madt.parse_interrupt_model_in(allocator()).expect("Interrupt model not found in MADT");
         let cpu_info = int_model.1.expect("CPU info not found in interrupt model");
 
