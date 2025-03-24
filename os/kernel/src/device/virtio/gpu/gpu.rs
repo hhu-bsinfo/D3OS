fde9c377598c51dfcfdfcbd123ec6fede707f501
nut97rif <elrich.nikitaaa@gmail.com>
2025-03-24 14:49:26 +0100

    Add GPU Command Structures

--- os/kernel/src/device/virtio/gpu/gpu.rs
+++ os/kernel/src/device/virtio/gpu/gpu.rs
@@ -12,10 +12,10 @@ use crate::device::virtio::transport::flags::DeviceStatusFlags;
 
 const VIRTIO_GPU_MAX_SCANOUTS: usize = 16;
 
-#repr(C)]
+#[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
-pub struct VirtioGpu {
-    pciDevice: RwLock<EndpointHeader>,
+struct VirtioGpu {
+    //pciDevice: RwLock<EndpointHeader>,
     rect: Option<VirtioGpuRect>,
     //frame_buffer: FrameBuffer
     // cursot_buffer not implemented
@@ -26,20 +26,20 @@ pub struct VirtioGpu {
 
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
-pub struct GpuConfig {
+struct GpuConfig {
     /// signals pending events to the driver. The driver MUST NOT write to this field.
-    pub events_read: u32,
+    events_read: u32,
     /// clears pending events in the device. Writing a ’1’ into a bit will clear the corresponding bit in events_read, mimicking write-to-clear behavior.
-    pub events_clear: u32,
+    events_clear: u32,
     /// specifies the maximum number of scanouts supported by the device. Minimum value is 1, maximum value is 16.
-    pub num_scanouts: u32,
+    num_scanouts: u32,
     /// specifies the maximum number of capability sets supported by the device. The minimum value is zero.
-    pub num_capsets: u32,
+    num_capsets: u32,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 #[repr(u32)]
-pub enum VirtioGpuCtrlType {
+enum VirtioGpuCtrlType {
     Undefined = 0,
 
     // 2D commands
@@ -97,66 +97,115 @@ const VIRTIO_GPU_FLAG_INFO_RING_IDX: u32 = 1 << 1;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct VirtioGpuCtrlHdr {
-    pub type_: VirtioGpuCtrlType, // Safe to use
-    pub flags: u32,
-    pub fence_id: u64,
-    pub ctx_id: u32,
-    pub ring_idx: u8,
-    pub padding: [u8; 3], // Explicit padding for struct alignment
+struct VirtioGpuCtrlHdr {
+    type_: VirtioGpuCtrlType,
+    flags: u32,
+    fence_id: u64,
+    ctx_id: u32,
+    ring_idx: u8,
+    _padding: u32,
 }
 
-#[repr(C)]
-#[derive(Debug, Clone, Copy)]
-pub struct VirtioGpuRect {
-    pub x: u32,
-    pub y: u32,
-    pub width: u32,
-    pub height: u32,
-}
-
-impl VirtioGpuRect {
-    pub fn new(x: u32, y: u32, width: u32, height: u32) -> Self {
-        Self { x, y, width, height }
-    }
-}
-
-#[repr(C)]
-#[derive(Debug, Clone, Copy)]
-pub struct VirtioGpuDisplayOne {
-    pub r: VirtioGpuRect,
-    pub enabled: u32,
-    pub flags: u32,
-}
-
-#[repr(C)]
-#[derive(Debug, Clone, Copy)]
-pub struct VirtioGpuRespDisplayInfo {
-    pub hdr: VirtioGpuCtrlHdr,
-    pub pmodes: [VirtioGpuDisplayOne; VIRTIO_GPU_MAX_SCANOUTS],
-}
-
-impl Default for VirtioGpuRespDisplayInfo {
-    fn default() -> Self {
+impl VirtioGpuCtrlHdr {
+    fn with_type(type_: VirtioGpuCtrlType) -> Self {
         Self {
-            hdr: VirtioGpuCtrlHdr {
-                type_: VirtioGpuCtrlType::GetDisplayInfo,
-                ..Default::default()
-            },
-
-            pmodes: unsafe { core::mem::zeroed() },
+            type_,
+            flags: 0,
+            fence_id: 0,
+            ctx_id: 0,
+            ring_idx: 0,
+            _padding: 0,
         }
     }
+
+    fn check_type(&self, type_: VirtioGpuCtrlType) -> bool {
+        self.type_ == type_
+    }
 }
 
-
-pub struct VirtioGpuDevice {
-    device: Arc<Mutex<EndpointHeader>>,
-    io_base: u16,
-    irq: u8,
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuRect {
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
 }
 
-impl VirtioGpuDevice {
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuRespDisplayInfo {
+    hdr: VirtioGpuCtrlHdr,
+    rect: VirtioGpuRect,
+    enabled: u32,
+    flags: u32,
+}
+
+#[repr(u32)]
+#[derive(Debug, Clone, Copy)]
+enum VirtioGpuFormats {
+    B8G8R8A8UNORM = 1,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct ResourceCreate2d {
+    hdr: VirtioGpuCtrlHdr,
+    resource_id: u32,
+    format: u32,
+    width: u32,
+    height: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuSetScanout {
+    hdr: VirtioGpuCtrlHdr,
+    r: VirtioGpuRect,
+    scanout_id: u32,
+    resource_id: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuResourceFlush {
+    hdr: VirtioGpuCtrlHdr,
+    r: VirtioGpuRect,
+    resource_id: u32,
+    _padding: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuTransferToHost2d {
+    hdr: VirtioGpuCtrlHdr,
+    r: VirtioGpuRect,
+    offset: u64,
+    resource_id: u32,
+    _padding: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct VirtioGpuResourceAttachBacking {
+    hdr: VirtioGpuCtrlHdr,
+    resource_id: u32,
+    nr_entries: u32,
+    addr: u64,
+    len: u32,
+    _padding: u32,
+}
+
+// Cursor Structs not implemented
+const QUEUE_TRANSMIT: u16 = 0;
+
+const SCANOUT_ID: u32 = 0;
+const RESOURCE_ID_FB: u32 = 0xbabe;
+
+
+
+
+/*impl VirtioGpuDevice {
     pub fn new(device: Arc<Mutex<EndpointHeader>>) -> Self {
         //let io_base = device.lock().bar(0, ()) as u16;
         //let irq = device.lock().interrupt_line();
@@ -189,7 +238,7 @@ impl VirtioGpuDevice {
     fn enable_interrupts(&self) {
         info!("Enabling interrupts for Virtio GPU...");
     }
-}
+}*/
 
 /*
 // GPU Commands
