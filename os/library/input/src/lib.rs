5e650bd76c481773f8ddde911520f3dae90eb19c
Léon Lehnen <leon.lehnen@hhu.de>
2025-03-30 18:15:38 +0200

    Fixed wrong calculation for signed y values

--- os/library/input/src/lib.rs
+++ os/library/input/src/lib.rs
@@ -15,15 +15,9 @@ impl MousePacket {
         let dy = (value >> 16) as u8;
         let _ = (value >> 24) as u8;
 
+        // Subtract 0x100 from dx and dy if the sign bit is set
         let dx : i16 = (dx as i16) - (((flags as i16) << 4) & 0x100);
-        let dy : i16 = (dy as i16) - (((flags as i16) << 5) & 0x100);
-        /*if flags.contains(MouseFlags::X_SIGNED) {
-            dx -= 0x100;
-        }
-
-        if flags.contains(MouseFlags::Y_SIGNED) {
-            dy -= 0x100;
-        }*/
+        let dy : i16 = (dy as i16) - (((flags as i16) << 3) & 0x100);
 
         Self { flags, dx, dy }
     }
