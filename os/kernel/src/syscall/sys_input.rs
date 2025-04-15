de44c6c581ad5adaada2d647ab614955b0dbbd9d
Léon Lehnen <leon.lehnen@hhu.de>
2025-04-15 14:48:14 +0200

    Switched to fixed ps2 crate repository

--- os/kernel/src/syscall/sys_input.rs
+++ os/kernel/src/syscall/sys_input.rs
@@ -1,8 +1,8 @@
 use crate::mouse;
 
 pub fn sys_read_mouse() -> usize {
-    match mouse().expect("Failed to read from mouse!").read() {
-        Some(v) => v as usize,
+    match mouse() {
+        Some(mouse) => mouse.read().unwrap_or(0x0) as usize,
         None => 0x0,
     }
 }
