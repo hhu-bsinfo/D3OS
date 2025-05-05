45f5e46cf1113a10ac7cacbf33bdc58ff3932e37
Sebastian Keller <sebastian.keller@hhu.de>
2025-05-05 10:52:19 +0200

    Fix compilation errors with syscalls and comment out

--- os/application/lfb_terminal/src/lfb_terminal.rs
+++ os/application/lfb_terminal/src/lfb_terminal.rs
@@ -4,11 +4,11 @@ extern crate alloc;
 
 mod terminal;
 
-use alloc::format;
+use alloc::string::ToString;
 use alloc::sync::Arc;
 use alloc::vec::Vec;
 use anstyle_parse::{Params, ParamsIter, Parser, Perform, Utf8Parser};
-use chrono::TimeDelta;
+use concurrent::thread;
 use core::cell::RefCell;
 use core::mem::size_of;
 use core::ptr;
@@ -22,6 +22,7 @@ use pc_keyboard::{DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use spin::Mutex;
 use stream::{InputStream, OutputStream};
 use terminal::Terminal;
+use time::date;
 
 const CURSOR: char = if let Some(cursor) = char::from_u32(0x2588) {
     cursor
@@ -141,7 +142,7 @@ impl CursorThread {
         let mut sleep_counter = 0usize;
 
         loop {
-            scheduler().sleep(CURSOR_UPDATE_INTERVAL);
+            thread::sleep(CURSOR_UPDATE_INTERVAL);
             sleep_counter += CURSOR_UPDATE_INTERVAL;
 
             // CAUTION: This only works because LFBTerminal is the only implementation of Terminal
@@ -217,32 +218,33 @@ impl OutputStream for LFBTerminal {
 
 impl InputStream for LFBTerminal {
     fn read_byte(&self) -> i16 {
-        if let Some(keyboard) = keyboard() {
-            let read_byte;
+        // TODO#1 Fix keyboard access
+        // if let Some(keyboard) = keyboard() {
+        //     let read_byte;
 
-            loop {
-                let mut decoder = self.decoder.lock();
-                let scancode = keyboard.read_byte();
-                if scancode == -1 {
-                    panic!("Keyboard stream closed!");
-                }
+        //     loop {
+        //         let mut decoder = self.decoder.lock();
+        //         let scancode = keyboard.read_byte();
+        //         if scancode == -1 {
+        //             panic!("Keyboard stream closed!");
+        //         }
 
-                if let Ok(Some(event)) = decoder.add_byte(scancode as u8) {
-                    if let Some(key) = decoder.process_keyevent(event) {
-                        match key {
-                            DecodedKey::Unicode(c) => {
-                                read_byte = c;
-                                break;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            }
+        //         if let Ok(Some(event)) = decoder.add_byte(scancode as u8) {
+        //             if let Some(key) = decoder.process_keyevent(event) {
+        //                 match key {
+        //                     DecodedKey::Unicode(c) => {
+        //                         read_byte = c;
+        //                         break;
+        //                     }
+        //                     _ => {}
+        //                 }
+        //             }
+        //         }
+        //     }
 
-            self.write_byte(read_byte as u8);
-            return read_byte as i16;
-        }
+        //     self.write_byte(read_byte as u8);
+        //     return read_byte as i16;
+        // }
 
         -1
     }
@@ -369,53 +371,43 @@ impl LFBTerminal {
             }
         }
 
+        // TODO#1 Fix system info access
         // Collect system information
-        let uptime = TimeDelta::try_milliseconds(timer().systime_ms() as i64)
-            .expect("Failed to create TimeDelta struct from systime");
-        let active_process_ids = process_manager().read().active_process_ids();
-        let active_thread_ids = scheduler().active_thread_ids();
+        // let uptime = TimeDelta::try_milliseconds(timer().systime_ms() as i64)
+        //     .expect("Failed to create TimeDelta struct from systime");
+        // let active_process_ids = process_manager().read().active_process_ids();
+        // let active_thread_ids = scheduler().active_thread_ids();
 
         // Draw info string
-        let info_string = format!(
-            "D³OS v{} ({}) | Uptime: {:0>2}:{:0>2}:{:0>2} | Processes: {} | Threads: {}",
-            built_info::PKG_VERSION,
-            built_info::PROFILE,
-            uptime.num_hours(),
-            uptime.num_minutes() % 60,
-            uptime.num_seconds() - (uptime.num_minutes() * 60),
-            active_process_ids.len(),
-            active_thread_ids.len()
-        );
+        // let info_string = format!(
+        //     "D³OS v{} ({}) | Uptime: {:0>2}:{:0>2}:{:0>2} | Processes: {} | Threads: {}",
+        //     built_info::PKG_VERSION,
+        //     built_info::PROFILE,
+        //     uptime.num_hours(),
+        //     uptime.num_minutes() % 60,
+        //     uptime.num_seconds() - (uptime.num_minutes() * 60),
+        //     active_process_ids.len(),
+        //     active_thread_ids.len()
+        // );
+
+        // display.lfb.lfb().draw_string(
+        //     0,
+        //     0,
+        //     color::HHU_BLUE,
+        //     color::INVISIBLE,
+        //     info_string.as_str(),
+        // );
+
+        // Draw date
+        let date_str = date().format("%Y-%m-%d %H:%M:%S").to_string();
 
         display.lfb.lfb().draw_string(
-            0,
+            (display.size.0 as u32 - date_str.len() as u32) * lfb::DEFAULT_CHAR_WIDTH,
             0,
             color::HHU_BLUE,
             color::INVISIBLE,
-            info_string.as_str(),
+            &date_str,
         );
-
-        // Draw date
-        if efi_services_available() {
-            if let Ok(date) = uefi::runtime::get_time() {
-                let date_str = format!(
-                    "{}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2}",
-                    date.year(),
-                    date.month(),
-                    date.day(),
-                    date.hour(),
-                    date.minute(),
-                    date.second()
-                );
-                display.lfb.lfb().draw_string(
-                    (display.size.0 as u32 - date_str.len() as u32) * lfb::DEFAULT_CHAR_WIDTH,
-                    0,
-                    color::HHU_BLUE,
-                    color::INVISIBLE,
-                    &date_str,
-                );
-            }
-        }
 
         display.lfb.flush_lines(0, lfb::DEFAULT_CHAR_HEIGHT);
     }
@@ -469,9 +461,10 @@ impl LFBTerminal {
     }
 
     fn handle_bell() {
-        let speaker = speaker();
-        speaker.play(440, 250);
-        speaker.play(880, 250);
+        // TODO#7 fix speaker access
+        // let speaker = speaker();
+        // speaker.play(440, 250);
+        // speaker.play(880, 250);
     }
 
     fn handle_tab(display: &mut DisplayState, cursor: &mut CursorState, color: &mut ColorState) {
