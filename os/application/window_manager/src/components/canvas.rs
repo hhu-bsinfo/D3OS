75d5fe8b90d6bc5ad1879d7377e3fda78be03eab
Julius Carl Drodofsky <judro102@hhu.de>
2025-05-06 17:39:48 +0200

    starts with implementing Component

--- os/application/window_manager/src/components/canvas.rs
+++ os/application/window_manager/src/components/canvas.rs
@@ -2,23 +2,67 @@
 
 pub struct Canvas {
     pub id: Option<usize>,
-    pub is_dirty: bool,
-    pub abs_pos: Vertex,
-    pub rel_pos: Vertex,
-    pub styling: ComponentStyling,
+    is_dirty: bool,
+    abs_pos: Vertex,
+    rel_pos: Vertex,
+    drawn_rect_data: RectData,
+    styling: ComponentStyling,
     buffer: Vec<u32>,
     widht: usize,
     height: usize,
-    ppb: u8,
+    // default 4
+    // bpp: u8,
 } 
 
 impl Canvas {
     pub fn new (
     abs_pos: Vertex,
     rel_pos: Vertex,
-    styling: Option<ComponentStyling>
+    styling: Option<ComponentStyling>,
+    width: usize,
+    height: usize,
     ) {
-        todo!()
+    let drawn_rect_data = RectData {
+         top_left: abs_center.sub(abs_radius, abs_radius),
+        width: width,
+        height: height,
+    };
+    Self {
+        id: None,
+        is_dirt: false,
+        abs_pos,
+        rel_pos,
+        drawn_rect_data,
+        styling: styling.unwrap_or_default(),
+        }
     }
 
+}
+
+impl Component for Canvas {
+    fn draw(&mut self, is_focused: bool) {
+        todo!()
+    }
+    fn is_dirty(&self) -> bool {
+        self.is_dirty
+    }
+
+    fn mark_dirty(&mut self) {
+        self.is_dirty = true;
+    }
+
+    fn get_id(&self) -> Option<usize> {
+        self.id
+    }
+
+    fn set_id(&mut self, id: usize) {
+        self.id = Some(id);
+    }
+    fn get_abs_rect_data(&self) -> RectData {
+       self.drawn_rect_data 
+    }
+
+    fn get_drawn_rect_data(&self) -> RectData {
+        self.drawn_rect_data
+    }
 }
