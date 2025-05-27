c3ede5bcd4336693aedf348c7771387757f0b822
Julius Carl Drodofsky <judro102@hhu.de>
2025-05-27 11:00:01 +0200

    run cargo fmt

--- os/application/window_manager/src/apps/text_editor.rs
+++ os/application/window_manager/src/apps/text_editor.rs
@@ -1,19 +1,24 @@
+use super::runnable::Runnable;
+use crate::apps::text_editor::view::View;
+use crate::{
+    alloc::string::ToString, components::component::ComponentStylingBuilder, config, signal::Signal,
+};
+use crate::{api::Command, WindowManager};
+use alloc::collections::VecDeque;
 use alloc::{borrow::ToOwned, boxed::Box, rc::Rc, string::String, vec};
-use model::Document;
-use terminal::DecodedKey;
-use crate::{alloc::string::ToString, components::component::ComponentStylingBuilder, config, signal::Signal};
-use graphic::{buffered_lfb, color::{Color, WHITE}};
-use spin::rwlock::RwLock;
 use drawer::{rect_data::RectData, vertex::Vertex};
 use graphic::{bitmap::Bitmap, lfb::DEFAULT_CHAR_HEIGHT};
-use super::runnable::Runnable;
-use crate::{api::Command, WindowManager};
-use crate::apps::text_editor::view::View;
+use graphic::{
+    buffered_lfb,
+    color::{Color, WHITE},
+};
+use model::Document;
+use spin::rwlock::RwLock;
+use terminal::DecodedKey;
 use text_buffer::TextBuffer;
-use alloc::collections::VecDeque;
 
-mod view;
 mod model;
+mod view;
 
 // Julius Drodofsky
 pub struct TextEditor;
@@ -24,46 +29,59 @@ pub struct TextEditorConfig {
     pub background_color: Color,
 }
 
-
 impl Runnable for TextEditor {
     fn run() {
-        let config = TextEditorConfig{widht: 720, height: 500, background_color: Color::new(20,20,20,255)};
+        let config = TextEditorConfig {
+            widht: 720,
+            height: 500,
+            background_color: Color::new(20, 20, 20, 255),
+        };
         let bitmap = Bitmap {
             width: config.widht as u32,
             height: config.height as u32,
-            data: vec![
-                config.background_color;
-                config.widht*config.height
-            ],
+            data: vec![config.background_color; config.widht * config.height],
         };
         let deque = VecDeque::<DecodedKey>::new();
-        let handle = concurrent::thread::current().expect("Failed to get thread").id();
+        let handle = concurrent::thread::current()
+            .expect("Failed to get thread")
+            .id();
         let api = WindowManager::get_api();
         let canvas = Rc::new(RwLock::new(bitmap));
         let input = Rc::new(RwLock::<VecDeque<DecodedKey>>::new(deque));
         let input_clone = Rc::clone(&input);
-        let component = api.execute(handle, None,  Command::CreateCanvas { styling: None,  rect_data: RectData {
-                    top_left: Vertex::new(50, 80),
-                    width: config.widht as u32,
-                    height: config.height as u32,
+        let component = api
+            .execute(
+                handle,
+                None,
+                Command::CreateCanvas {
+                    styling: None,
+                    rect_data: RectData {
+                        top_left: Vertex::new(50, 80),
+                        width: config.widht as u32,
+                        height: config.height as u32,
+                    },
+                    input: Some(Box::new(move |c: DecodedKey| {
+                        input_clone.write().push_back(c);
+                    })),
+                    buffer: Rc::clone(&canvas),
                 },
-                input: Some(Box::new(move |c: DecodedKey| {
-                    input_clone.write().push_back(c);
-                })),
-                buffer: Rc::clone(&canvas),
-            }).unwrap();
-        
+            )
+            .unwrap();
 
         let mut text_buffer = TextBuffer::from_str("Das ist ein Text!");
         let mut document: Document = Document::new(Some(String::from("scratch")), text_buffer);
-        
-        let mut view = View::Simple{font_scale: 1, fg_color: WHITE, bg_color: config.background_color};
+
+        let mut view = View::Simple {
+            font_scale: 1,
+            fg_color: WHITE,
+            bg_color: config.background_color,
+        };
         view.render(&document, &mut canvas.write());
         component.write().mark_dirty();
         let mut dirty = false;
         loop {
             let mut tmp_queue = VecDeque::<DecodedKey>::new();
-            while let Some(value) = input.write().pop_front(){
+            while let Some(value) = input.write().pop_front() {
                 tmp_queue.push_back(value);
                 dirty = true;
             }
@@ -77,7 +95,7 @@ impl Runnable for TextEditor {
                 }
                 component.write().mark_dirty();
                 dirty = false;
-            }   
+            }
         }
     }
 }
--- os/application/window_manager/src/apps/text_editor/model.rs
+++ os/application/window_manager/src/apps/text_editor/model.rs
@@ -1,9 +1,9 @@
+use alloc::string::String;
 use concurrent::thread::switch;
 use logger::{error, warn};
+use terminal::print;
 use terminal::{println, DecodedKey};
 use text_buffer::TextBuffer;
-use alloc::string::String;
-use terminal::print;
 use time::set_date;
 
 enum EditMode {
@@ -11,18 +11,22 @@ enum EditMode {
     Insert,
 }
 
-pub struct Document<'b>{
+pub struct Document<'b> {
     path: Option<String>,
     text_buffer: TextBuffer<'b>,
     caret: usize,
     edit_mode: EditMode,
 }
 
-
-impl <'b>Document<'b> {
+impl<'b> Document<'b> {
     pub fn new(path: Option<String>, text_buffer: TextBuffer<'b>) -> Document<'b> {
         let length = text_buffer.len();
-        Document {path: path, text_buffer: text_buffer, caret: length , edit_mode: EditMode::Insert}
+        Document {
+            path: path,
+            text_buffer: text_buffer,
+            caret: length,
+            edit_mode: EditMode::Insert,
+        }
     }
     pub fn text_buffer(&self) -> &TextBuffer {
         &self.text_buffer
@@ -32,48 +36,48 @@ impl <'b>Document<'b> {
     }
     fn next_line(&self, pos: usize) -> Option<usize> {
         let mut index = 0;
-        while let Some(c) = self.text_buffer.get_char(pos+index){
-            if c == '\n'{
+        while let Some(c) = self.text_buffer.get_char(pos + index) {
+            if c == '\n' {
                 break;
             }
-            index +=1;
+            index += 1;
         }
-        if self.text_buffer.get_char(pos+index+1).is_some() {
-            return Some(pos+index+1);
+        if self.text_buffer.get_char(pos + index + 1).is_some() {
+            return Some(pos + index + 1);
         }
         None
-    } 
+    }
     fn prev_line(&self, pos: usize) -> Option<usize> {
         let mut index = 0;
-        while let Some(c) = self.text_buffer.get_char(pos-index){
-            if c == '\n'{
+        while let Some(c) = self.text_buffer.get_char(pos - index) {
+            if c == '\n' {
                 break;
             }
-            index +=1;
+            index += 1;
             if pos < index {
                 break;
             }
         }
-        if self.text_buffer.get_char(pos-index+1).is_some() {
-            return Some(pos-index+1);
+        if self.text_buffer.get_char(pos - index + 1).is_some() {
+            return Some(pos - index + 1);
         }
         None
     }
 
-    fn move_cursor_down(&mut self){
+    fn move_cursor_down(&mut self) {
         let text = self.text_buffer.to_string();
         let prev_line = self.prev_line(self.caret).unwrap_or(self.caret);
         let origin_len = self.caret - prev_line;
         let next_line = self.next_line(self.caret).unwrap_or(self.caret);
-        self.caret = next_line + origin_len; 
+        self.caret = next_line + origin_len;
     }
 
-    fn move_cursor_up(&mut self){
+    fn move_cursor_up(&mut self) {
         let text = self.text_buffer.to_string();
         let prev_line = self.prev_line(self.caret).unwrap_or(self.caret);
         let origin_len = self.caret - prev_line;
-        let prev_prev_line = self.prev_line(prev_line-2).unwrap_or(self.caret);
-        self.caret = prev_prev_line + origin_len; 
+        let prev_prev_line = self.prev_line(prev_line - 2).unwrap_or(self.caret);
+        self.caret = prev_prev_line + origin_len;
     }
 
     fn update_insert(&mut self, k: DecodedKey) {
@@ -81,38 +85,42 @@ impl <'b>Document<'b> {
         match k {
             // delete
             DecodedKey::Unicode('\x08') => {
-                self.text_buffer.delete(self.caret-1);
-                self.caret -=1;
-            }, 
+                self.text_buffer.delete(self.caret - 1);
+                self.caret -= 1;
+            }
             // esc
             DecodedKey::Unicode('\x1B') => {
                 self.edit_mode = EditMode::Normal;
             }
             DecodedKey::Unicode(ch) => {
                 self.text_buffer.insert(self.caret, ch);
-                self.caret+=1;
+                self.caret += 1;
             }
-            DecodedKey::RawKey(terminal::KeyCode::ArrowLeft) => self.caret -=1,
-            DecodedKey::RawKey(terminal::KeyCode::ArrowRight) => self.caret +=1,
+            DecodedKey::RawKey(terminal::KeyCode::ArrowLeft) => self.caret -= 1,
+            DecodedKey::RawKey(terminal::KeyCode::ArrowRight) => self.caret += 1,
             DecodedKey::RawKey(terminal::KeyCode::ArrowUp) => self.move_cursor_up(),
             DecodedKey::RawKey(terminal::KeyCode::ArrowDown) => self.move_cursor_down(),
-            DecodedKey::RawKey(k) => warn!("TextEditor can't process input: {:?}", k)
+            DecodedKey::RawKey(k) => warn!("TextEditor can't process input: {:?}", k),
         }
     }
 
     fn update_normal(&mut self, k: DecodedKey) {
         // funktioniert irgendwie nicht
-        println!("Ausgabe: {}",self.text_buffer.to_string());
+        println!("Ausgabe: {}", self.text_buffer.to_string());
         match k {
-            DecodedKey::Unicode('u') => {self.text_buffer.undo();},
-            DecodedKey::Unicode('r') => {self.text_buffer.redo();},
-            DecodedKey::Unicode('h') => self.caret -=1,
-            DecodedKey::Unicode('l') => self.caret +=1,
+            DecodedKey::Unicode('u') => {
+                self.text_buffer.undo();
+            }
+            DecodedKey::Unicode('r') => {
+                self.text_buffer.redo();
+            }
+            DecodedKey::Unicode('h') => self.caret -= 1,
+            DecodedKey::Unicode('l') => self.caret += 1,
             DecodedKey::Unicode('j') => self.move_cursor_down(),
             DecodedKey::Unicode('k') => self.move_cursor_up(),
             DecodedKey::Unicode('i') => self.edit_mode = EditMode::Insert,
-            DecodedKey::RawKey(terminal::KeyCode::ArrowLeft) => self.caret -=1,
-            DecodedKey::RawKey(terminal::KeyCode::ArrowRight) => self.caret +=1,
+            DecodedKey::RawKey(terminal::KeyCode::ArrowLeft) => self.caret -= 1,
+            DecodedKey::RawKey(terminal::KeyCode::ArrowRight) => self.caret += 1,
             DecodedKey::RawKey(terminal::KeyCode::ArrowUp) => self.move_cursor_up(),
             DecodedKey::RawKey(terminal::KeyCode::ArrowDown) => self.move_cursor_down(),
             _ => (),
@@ -127,6 +135,5 @@ impl <'b>Document<'b> {
             EditMode::Insert => self.update_insert(k),
             EditMode::Normal => self.update_normal(k),
         }
-        
     }
 }
