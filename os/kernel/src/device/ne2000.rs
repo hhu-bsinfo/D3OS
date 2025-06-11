160152cf580ddd8b24f1c3449ef9bce42637fe83
alex <alexdot1@protonmail.com>
2025-06-11 11:33:50 +0200

    checked registers after reset, looking for  a solution for the Problem, in which CR, IMR, DCR and DCR don't get set properly

--- os/kernel/src/device/ne2000.rs
+++ os/kernel/src/device/ne2000.rs
@@ -53,7 +53,7 @@ impl Ne2000 {
     pub fn new(pci_device: &RwLock<EndpointHeader>) -> Self {
         info!("Configuring PCI registers");
         //Self { base_address }
-        //let pci_config_space = pci_bus().config_space();
+        let pci_config_space = pci_bus().config_space();
         let pci_device = pci_device.write();
 
         let bar0 = pci_device
@@ -70,34 +70,116 @@ impl Ne2000 {
     pub fn read_mac(&self) -> [u8; 6] {
         //pub fn read_mac(&self) -> EthernetAddress {
         let mut mac = [0u8; 6];
+        let mut mac2 = [0u8; 6];
 
         unsafe {
             // Define ports
 
             // reset port
-            let mut reset_port = Port::<u8>::new(self.base_address + RESET as u16);
+            let mut reset_port = Port::<u8>::new(self.base_address + 0x1F);
             let mut command_port = Port::<u8>::new(self.base_address + 0x00);
             let mut rsar0 = Port::<u8>::new(self.base_address + 0x08);
             let mut rsar1 = Port::<u8>::new(self.base_address + 0x09);
             let mut rbcr0 = Port::<u8>::new(self.base_address + 0x0A);
             let mut rbcr1 = Port::<u8>::new(self.base_address + 0x0B);
             let mut data_port = Port::<u8>::new(self.base_address + 0x10);
+            let mut isr_port = Port::<u8>::new(self.base_address + 0x07);
+            let mut rst_port = Port::<u8>::new(self.base_address + 0x80);
+            let mut imr_port = Port::<u8>::new(self.base_address + 0x0F);
+            let mut dcr_port = Port::<u8>::new(self.base_address + 0x0E);
+            let mut tcr_port = Port::<u8>::new(self.base_address + 0x0D);
+            let mut rcr_port = Port::<u8>::new(self.base_address + 0x0C);
+
+            info!("Powering on device");
+            //command_port.write(0x02);
+            let j = isr_port.read();
+            info!("ISR: {}", j);
+            let d = command_port.read();
+            info!("CR: {}", d);
+            let e = imr_port.read();
+            info!("IMR: {}", e);
+            let f = dcr_port.read();
+            info!("DCR: {}", f);
+            let g = tcr_port.read();
+            info!("TCR: {}", g);
+            info!("Resetting Device NE2000");
+
+            //reset_port.write(0);
+
+            // just doing the read operation enables the reset, a write is not necessary, but the bits dont get set correctly
+            // see spec in PDF
+            //TODO:, add comments what registers are affected and which bits are set
+            let a = reset_port.read();
+            info!("1: 0x{:X}", a);
+            //reset_port.write(a);
+            let c = isr_port.read();
+            info!("ISR: 0x{:X}", c);
+
+            while (isr_port.read() & 0x80) == 0 {
+                //info!("Reset in Progress");
+            }
+            isr_port.write(c);
+            info!("NE2000 reset complete");
+
+            let j = isr_port.read();
+            info!("ISR: 0x{:X}", j);
+            let d = command_port.read();
+            info!("CR: {}", d);
+            let e = imr_port.read();
+            info!("IMR: {}", e);
+            let f = dcr_port.read();
+            info!("DCR: 0x{:X}", f);
+            let g = tcr_port.read();
+            info!("TCR: {}", g);
+
+            info!("Initializing Device NE2000");
+
+            // Initialize CR Register
+            //command_port.write(0x21);
+            //scheduler().sleep(100);
+
+            //info!("cr: {}", command_port.read());
+
+            dcr_port.write(0x58);
+            info!("dcr: {}", dcr_port.read());
+            info!("DCR: 0x{:04X}", dcr_port.read());
+
+            // clear RBCR1,0
+            //RBCR0,1 : indicates the length of the block in bytes
+            // MAC address has length of 6 Bytes
+            rbcr0.write(0x00);
+            rbcr1.write(0x00);
+            info!("rb0: {}", rbcr0.read());
+            info!("rb1: {}", rbcr1.read());
+
+            // initialize RCR
+            rcr_port.write(0x14);
+            info!("rcr: {}", rcr_port.read());
 
             //Reset the NIC
             // Clears the Registers CR, ISR, IMR, DCR, TCR (see NS32490D.pdf, p.29, 11.0 Initialization Procedure)
             // this ensures, that the Registers are cleared and no undefined behavior can happen
-            info!("Resetting Device NE2000");
-            reset_port.read();
+            //registers.config1.write(0x00);
+            //reset_port.read();
+
+            // From C++ Ne2000
+            /** Wait until Reset Status is 0 */
+            //while(!(baseRegister.readByte(P0_ISR) & ISR_RST)) {
+            //Util::Async::Thread::sleep(Util::Time::Timestamp::ofMilliseconds(1));
+            //}
+            // Wait for the reset to complete
+            //reset_port.write(0x00);
 
             //Set up Remote DMA to read from address 0x0000
             // RSAR0,1 : points to the start of the block of data to be transfered
-            rsar0.write(0x00);
-            rsar1.write(0x00);
-
-            //RBCR0,1 : indicates the length of the block in bytes
-            // MAC address has length of 6 Bytes
+            rsar0.write(0);
+            rsar1.write(0);
+            info!("rsa0: {}", rsar0.read());
+            info!("rsa1: {}", rsar1.read());
             rbcr0.write(6);
             rbcr1.write(0);
+            info!("rb0: {}", rbcr0.read());
+            info!("rb1: {}", rbcr1.read());
 
             //Issue Remote Read command
             // Command Port is 8 Bits and has the following structure
@@ -114,12 +196,8 @@ impl Ne2000 {
             // The first 16 bytes contains the MAC address at even locations,
             //command_port.write(0x0A);
             //command_port.write(0x20);
-            let cr: u8 = unsafe { command_port.read() };
-            info!("Page: ({}) ", cr);
-
-            for _ in 0..6 {
-                let _ = data_port.read();
-            } // discard
+            //let cr: u8 = unsafe { command_port.read() };
+            //info!("Page: ({}) ", cr);
 
             //Read 6 bytes (MAC address)
             for byte in mac.iter_mut() {
@@ -141,20 +219,24 @@ impl Ne2000 {
             ];
             for (i, port) in par_ports.iter_mut().enumerate() {
                 mac[i] = port.read();
+                mac2[i] = port.read();
             }
 
-            unsafe {
-                let mut command_port = Port::<u8>::new(self.base_address + 0x00);
-                let cr = command_port.read();
-                let ps = (cr >> 6) & 0b11;
+            let address3 = EthernetAddress::from_bytes(&mac2);
+            info!("after mac 2 ({})", address3);
 
-                match ps {
-                    0 => info!("Currently on Page 0"),
-                    1 => info!("Currently on Page 1"),
-                    2 => info!("Currently on Page 2"),
-                    3 => info!("Currently on Page 3"),
-                    _ => unreachable!(),
-                }
+            // check if on correct Page (on Page 1 are the PARs Registers for the MAC Adress)
+
+            let mut command_port = Port::<u8>::new(self.base_address + 0x00);
+            let cr = command_port.read();
+            let ps = (cr >> 6) & 0b11;
+
+            match ps {
+                0 => info!("Currently on Page 0"),
+                1 => info!("Currently on Page 1"),
+                2 => info!("Currently on Page 2"),
+                3 => info!("Currently on Page 3"),
+                _ => unreachable!(),
             }
 
             let address2 = EthernetAddress::from_bytes(&mac);
