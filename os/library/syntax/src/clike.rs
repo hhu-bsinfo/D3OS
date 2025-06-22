ecdbd8d77dfa8a142f3bf0838653379ff047d96a
Julius Carl Drodofsky <judro102@hhu.de>
2025-06-22 12:13:34 +0200

    update cargo

---
+++ os/library/syntax/src/clike.rs
@@ -0,0 +1,79 @@
+use crate::located::Located;
+use nom::Err;
+use nom::error::Error;
+use nom::error::ErrorKind;
+use nom::{
+    IResult, Parser,
+    bytes::complete::{tag, take_while1},
+    character::complete::{alphanumeric1, digit1},
+    combinator::{not, peek},
+    sequence::{preceded, terminated},
+};
+
+// use located_token to always know where a Token is from
+pub type LToken<'s> = Located<Token<'s>, &'s str>;
+pub enum Token<'s> {
+    Keyword(&'s str),
+    Identifier(&'s str),
+    Number(&'s str),
+    Operator(&'s str),
+    Punctuation(&'s str),
+    Whitespace(&'s str),
+}
+
+pub fn match_any<'arr, 's>(
+    tags: &'arr [&'s str],
+) -> impl FnMut(&'s str) -> IResult<&'s str, &'s str> {
+    move |input: &'s str| {
+        for &kw in tags {
+            if let Ok((rest, _)) = tag::<_, _, Error<&str>>(kw)(input) {
+                return Ok((rest, kw));
+            }
+        }
+        Err(Err::Error(Error::new(input, ErrorKind::Tag)))
+    }
+}
+
+fn keyword<'a, 's>(input: &'s str, keywords: &'a [&'s str]) -> IResult<&'s str, &'s str> {
+    terminated(match_any(keywords), peek(not(alphanumeric1))).parse(input)
+}
+
+fn identifier(input: &str) -> IResult<&str, &str> {
+    preceded(
+        not(digit1),
+        take_while1(|c: char| c.is_alphanumeric() || c == '_'),
+    )
+    .parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn keyword_0() {
+        let tags = &["int", "return", "if"];
+        assert_eq!(keyword("int x", tags), Ok((" x", "int")));
+    }
+    #[test]
+    fn keyword_prefix_int() {
+        let tags = &["int", "return", "if"];
+        assert_eq!(
+            keyword("return42", tags),
+            Err(nom::Err::Error(nom::error::Error::new(
+                "42",
+                nom::error::ErrorKind::Not
+            )))
+        ); // not followed by boundary
+    }
+    #[test]
+    fn keyword_prefix_punc() {
+        let tags = &["int", "return", "if"];
+        assert_eq!(keyword("if(", tags), Ok(("(", "if")));
+    }
+    #[test]
+    fn keyword_prefix_alph() {
+        let tags = &["int", "return", "if"];
+        assert!(keyword("introspect", tags).is_err());
+    }
+}
---
+++ os/library/syntax/src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+
+#[cfg(not(feature = "alloc"))]
+compile_error!("The 'alloc' feature must be enabled.");
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+mod clike;
+mod located;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
