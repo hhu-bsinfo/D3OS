cf3953bc8374b0f126251372c3e2b4466406acba
Sebastian Keller <sebastian.keller@hhu.de>
2025-07-28 12:26:33 +0200

    Add line editing to canonical mode

--- os/application/terminal_emulator/src/worker/input_observer.rs
+++ os/application/terminal_emulator/src/worker/input_observer.rs
@@ -1,8 +1,8 @@
 use core::cell::RefCell;
 
-use alloc::{rc::Rc, vec::Vec};
+use alloc::{format, rc::Rc, string::String, vec::Vec};
 use globals::hotkeys::HKEY_TOGGLE_TERMINAL_WINDOW;
-use pc_keyboard::DecodedKey;
+use pc_keyboard::{DecodedKey, KeyCode};
 use stream::{InputStream, OutputStream};
 use syscall::{SystemCall, syscall};
 use terminal_lib::{DecodedKeyType, TerminalInputState, TerminalMode};
@@ -15,12 +15,87 @@ use crate::{
 
 use super::worker::Worker;
 
+const BUFFER_SIZE: usize = 256;
+
+struct Canonical {
+    cursor_pos: usize,
+    buffer: String,
+}
+
+impl Canonical {
+    const fn new() -> Self {
+        Self {
+            cursor_pos: 0,
+            buffer: String::new(),
+        }
+    }
+
+    fn submit(&mut self) -> Vec<u8> {
+        let buffer = self.buffer.clone();
+        self.cursor_pos = 0;
+        self.buffer.clear();
+        buffer.into()
+    }
+
+    fn remove_at_cursor(&mut self) -> Result<(), ()> {
+        if self.cursor_pos >= self.buffer.len() || self.buffer.is_empty() {
+            return Err(());
+        }
+        self.buffer.remove(self.cursor_pos);
+        Ok(())
+    }
+
+    fn remove_before_cursor(&mut self) -> Result<(), ()> {
+        if self.cursor_pos <= 0 {
+            return Err(());
+        }
+        self.buffer.remove(self.cursor_pos - 1);
+        self.cursor_pos -= 1;
+        Ok(())
+    }
+
+    fn add_at_cursor(&mut self, ch: char) -> Result<(), ()> {
+        if self.buffer.len() >= BUFFER_SIZE {
+            return Err(());
+        }
+        self.buffer.insert(self.cursor_pos, ch);
+        self.cursor_pos += 1;
+        Ok(())
+    }
+
+    fn move_cursor_to_start(&mut self) -> Result<usize, ()> {
+        self.cursor_pos = 0;
+        Ok(self.cursor_pos)
+    }
+
+    fn move_cursor_to_end(&mut self) -> Result<usize, ()> {
+        self.cursor_pos = self.buffer.len();
+        Ok(self.buffer.len() - self.cursor_pos)
+    }
+
+    fn move_cursor_left(&mut self) -> Result<(), ()> {
+        if self.cursor_pos <= 0 {
+            return Err(());
+        }
+        self.cursor_pos -= 1;
+        Ok(())
+    }
+
+    fn move_cursor_right(&mut self) -> Result<(), ()> {
+        if self.cursor_pos >= self.buffer.len() {
+            return Err(());
+        }
+        self.cursor_pos += 1;
+        Ok(())
+    }
+}
+
 pub struct InputObserver {
     terminal: Rc<LFBTerminal>,
     event_handler: Rc<RefCell<EventHandler>>,
     decoder: Decoder,
     mode: TerminalMode,
-    canonical_buffer: Vec<u8>,
+    canonical: Canonical,
 }
 
 impl InputObserver {
@@ -30,7 +105,7 @@ impl InputObserver {
             event_handler,
             decoder: Decoder::new(),
             mode: TerminalMode::Raw,
-            canonical_buffer: Vec::new(),
+            canonical: Canonical::new(),
         }
     }
 }
@@ -87,28 +162,68 @@ impl InputObserver {
         }
     }
 
-    // TODO add command line editing
     fn buffer_canonical(&mut self, key: DecodedKey) -> Option<Vec<u8>> {
-        let ch = match key {
-            DecodedKey::Unicode(ch) => ch,
-            _ => return None,
-        };
-
-        self.terminal.write_byte(ch as u8);
-
-        match ch {
-            '\n' => {
-                let buffer = self.canonical_buffer.clone();
-                self.canonical_buffer.clear();
-                return Some(buffer);
-            }
-            '\x08' => {
-                if self.canonical_buffer.pop().is_some() {
-                    self.terminal.write_str("\x1b[1D \x1b[1D");
+        match key {
+            DecodedKey::RawKey(KeyCode::ArrowLeft) => {
+                if self.canonical.move_cursor_left().is_ok() {
+                    self.terminal.write_str("\x1b[1D");
                 }
             }
-            _ => self.canonical_buffer.push(ch as u8),
+            DecodedKey::RawKey(KeyCode::ArrowRight) => {
+                if self.canonical.move_cursor_right().is_ok() {
+                    self.terminal.write_str("\x1b[1C");
+                }
+            }
+            DecodedKey::RawKey(KeyCode::Home) => {
+                if let Ok(steps) = self.canonical.move_cursor_to_start() {
+                    self.terminal.write_str(&format!("\x1b[{}D", steps));
+                }
+            }
+            DecodedKey::RawKey(KeyCode::End) => {
+                if let Ok(steps) = self.canonical.move_cursor_to_end() {
+                    self.terminal.write_str(&format!("\x1b[{}C", steps));
+                }
+            }
+            DecodedKey::RawKey(_) => return None,
+
+            DecodedKey::Unicode('\x1B') => return None,
+            DecodedKey::Unicode('\n') => {
+                let offset = self.canonical.buffer.len() - self.canonical.cursor_pos;
+                if offset > 0 {
+                    self.terminal.write_str(&format!("\x1B[{}C\n", offset));
+                } else {
+                    self.terminal.write_byte(b'\n');
+                }
+                return Some(self.canonical.submit());
+            }
+            DecodedKey::Unicode('\x08') => {
+                if self.canonical.remove_before_cursor().is_ok() {
+                    self.terminal
+                        .write_str(&format!("\x1B[1D \x1B[1D{}", self.redraw_canonical_content()));
+                }
+            }
+            DecodedKey::Unicode('\x7F') => {
+                if self.canonical.remove_at_cursor().is_ok() {
+                    self.terminal
+                        .write_str(&format!(" \x1B[1D{}", self.redraw_canonical_content()));
+                }
+            }
+            DecodedKey::Unicode(ch) => {
+                if self.canonical.add_at_cursor(ch).is_ok() {
+                    self.terminal
+                        .write_str(&format!("{}{}", ch, self.redraw_canonical_content()));
+                }
+            }
         };
         None
+    }
+
+    fn redraw_canonical_content(&self) -> String {
+        let content = &self.canonical.buffer[self.canonical.cursor_pos..];
+        if content.is_empty() {
+            String::new()
+        } else {
+            format!("\x1b[0K{}\x1B[{}D", content, content.len())
+        }
     }
 }
