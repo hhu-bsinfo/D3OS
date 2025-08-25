4c65d77ab22b11d23581c6f4fd416787715a2f7d
schoettner <michael.schoettner@hhu.de>
2025-08-25 07:23:39 +0200

    updated comments in scheduler.rs

--- os/kernel/src/process/scheduler.rs
+++ os/kernel/src/process/scheduler.rs
@@ -1,7 +1,21 @@
 /* ╔═════════════════════════════════════════════════════════════════════════╗
    ║ Module: scheduler                                                       ║
    ╟─────────────────────────────────────────────────────────────────────────╢
-   ║ Descr.: Implementation of the scheduler.                                ║
+   ║ Implementation of a basic round-robin scheduler.                        ║
+   ║                                                                         ║
+   ║ Public functions                                                        ║
+   ║   - active_thread_ids      get a list of all active thread IDs          ║
+   ║   - current_thread         get the currently running thread             ║
+   ║   - exit                   exit the calling thread                      ║
+   ║   - join                   wait for a thread to finish                  ║
+   ║   - kill                   kill a thread                                ║
+   ║   - set_init               set the scheduler as initialized             ║
+   ║   - thread                 get reference to a thread                    ║
+   ║   - ready                  insert a thread in the ready queue           ║
+   ║   - sleep                  put the caller into sleeping mode            ║
+   ║   - start                  start the scheduler                          ║
+   ║   - switch_thread_from_interrupt  switch thread, called from interrupt  ║
+   ║   - switch_thread_no_interrupt    switch thread, not called from int.   ║
    ╟─────────────────────────────────────────────────────────────────────────╢
    ║ Author: Fabian Ruhland, HHU                                             ║
    ╚═════════════════════════════════════════════════════════════════════════╝
@@ -25,7 +39,7 @@ pub fn next_thread_id() -> usize {
     THREAD_ID_COUNTER.fetch_add(1, Relaxed)
 }
 
-/// Everything related to the ready state in the scheduler
+/// Everything related to the threads in ready state in the scheduler
 struct ReadyState {
     initialized: bool,
     current_thread: Option<Arc<Thread>>,
@@ -60,7 +74,7 @@ pub unsafe extern "C" fn unlock_scheduler() {
 
 impl Scheduler {
 
-    /// Description: Create and init the scheduler.
+    /// Create and initialize the scheduler.
     pub fn new() -> Self {
         Self {
             ready_state: Mutex::new(ReadyState::new()),
@@ -69,11 +83,12 @@ impl Scheduler {
         }
     }
 
-    /// Description: Called during creation of threads
+    /// Called after the scheduler has been fully initialized
     pub fn set_init(&self) {
         self.get_ready_state().initialized = true;
     }
 
+    /// Get all active thread IDs
     pub fn active_thread_ids(&self) -> Vec<usize> {
         let state = self.get_ready_state();
         let sleep_list = self.sleep_list.lock();
@@ -86,13 +101,13 @@ impl Scheduler {
             .collect()
     }
 
-    /// Description: Return reference to current thread
+    /// Return reference to current thread
     pub fn current_thread(&self) -> Arc<Thread> {
         let state = self.get_ready_state();
         Scheduler::current(&state)
     }
 
-    /// Description: Return reference to thread for the given `thread_id`
+    /// Return reference to thread identified by `thread_id`
     pub fn thread(&self, thread_id: usize) -> Option<Arc<Thread>> {
         self.ready_state.lock().ready_queue
             .iter()
@@ -100,7 +115,7 @@ impl Scheduler {
             .cloned()
     }
 
-    /// Description: Start the scheduler, called only once from `boot.rs` 
+    /// Start the scheduler, called only once from `boot.rs` 
     pub fn start(&self) {
         // TODO: make sure this is actually called just once
         let mut state = self.get_ready_state();
@@ -109,11 +124,7 @@ impl Scheduler {
         unsafe { Thread::start_first(state.current_thread.as_ref().expect("Failed to dequeue first thread!").as_ref()); }
     }
 
-    /// 
-    /// Description: Insert a thread into the ready_queue
-    /// 
-    /// Parameters: `thread` thread to be inserted.
-    /// 
+    /// Insert `thread` into the ready queue of the scheduler
     pub fn ready(&self, thread: Arc<Thread>) {
         let id = thread.id();
 
@@ -134,7 +145,7 @@ impl Scheduler {
         join_map.insert(id, Vec::new());
     }
 
-    /// Description: Put calling thread to sleep for `ms` milliseconds
+    /// Put calling thread `self` to sleep for `ms` milliseconds
     pub fn sleep(&self, ms: usize) {
         let mut state = self.get_ready_state();
 
@@ -158,12 +169,8 @@ impl Scheduler {
         }
     }
 
-    /// 
-    /// Description: Switch from current to next thread (from ready queue)
-    /// 
-    /// Parameters: `interrupt` true = called from ISR -> need to send EOI to APIC
-    ///                         false = no EOI needed
-    /// 
+    /// Switch from current to next thread (from ready queue). \
+    /// If `interrupt` is true, the function is called from an ISR and will send EOI to APIC otherwise not. 
     fn switch_thread(&self, interrupt: bool) {
         if let Some(mut state) = self.ready_state.try_lock() {
             if !state.initialized {
@@ -204,21 +211,17 @@ impl Scheduler {
         }
     }
 
-    /// Description: helper function, calling `switch_thread`
+    /// Helper function for switching a thread not caused by an interrupt
     pub fn switch_thread_no_interrupt(&self) {
         self.switch_thread(false);
     }
 
-    /// Description: helper function, calling `switch_thread`
+    /// Helper function for switching a thread caused by an interrupt
     pub fn switch_thread_from_interrupt(&self) {
         self.switch_thread(true);
     }
 
-    /// 
-    /// Description: Calling thread wants to wait for another thread to terminate
-    /// 
-    /// Parameters: `thread_id` thread to wait for
-    /// 
+    /// Calling thread will block until thread with `thread_id` has terminated
     pub fn join(&self, thread_id: usize) {
         let mut state = self.get_ready_state();
         let thread = Scheduler::current(&state);
@@ -237,7 +240,7 @@ impl Scheduler {
         self.block(&mut state);
     }
 
-    /// Description: Exit calling thread.
+    /// Exit calling thread.
     pub fn exit(&self) -> ! {
         let mut ready_state;
         let current;
@@ -263,11 +266,7 @@ impl Scheduler {
         unreachable!()
     }
 
-    /// 
-    /// Description: Kill the thread with the  given id
-    /// 
-    /// Parameters: `thread_id` thread to be killed
-    /// 
+    /// Kill the thread with the id `thread_id`.
     pub fn kill(&self, thread_id: usize) {
         {
             // Check if current thread tries to kill itself (illegal)
@@ -293,12 +292,8 @@ impl Scheduler {
         ready_state.ready_queue.retain(|thread| thread.id() != thread_id);
     }
 
-    /// 
-    /// Description: Block calling thread
-    /// 
-    /// Parameters: `state` ReadyState of scheduler 
-    /// MS -> why this param?
-    /// 
+    /// Block calling thread. \
+    /// Function is used by `sleep` and `exit`
     fn block(&self, state: &mut ReadyState) {
         let mut next_thread = state.ready_queue.pop_back();
 
@@ -330,11 +325,12 @@ impl Scheduler {
         }
     }
 
-    /// Description: Return current running thread
+    /// Return current running thread
     fn current(state: &ReadyState) -> Arc<Thread> {
         Arc::clone(state.current_thread.as_ref().expect("Trying to access current thread before initialization!"))
     }
 
+    /// Check sleep list for threads that need to be waken up
     fn check_sleep_list(state: &mut ReadyState, sleep_list: &mut Vec<(Arc<Thread>, usize)>) {
         let time = timer().systime_ms();
 
@@ -348,7 +344,7 @@ impl Scheduler {
         });
     }
 
-    /// Description: Helper function returning `ReadyState` of scheduler in a MutexGuard
+    /// Helper function returning `ReadyState` of scheduler in a MutexGuard
     fn get_ready_state(&self) -> MutexGuard<'_, ReadyState> {
         let state;
 
