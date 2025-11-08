e136fb4db5701eccb5606fb84190b62ac2c24809
SandroMiudo <sandromiudo0@gmail.com>
2025-11-08 15:28:06 +0100

    adapt runtime to call with appropriate options

--- os/library/runtime/src/lib.rs
+++ os/library/runtime/src/lib.rs
@@ -12,6 +12,7 @@ extern crate alloc;
 pub mod env;
 
 use concurrent::{process, thread};
+use mm::MmapFlags;
 use core::panic::PanicInfo;
 use terminal::{print, println};
 use linked_list_allocator::LockedHeap;
@@ -32,8 +33,11 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 extern "C" fn entry() {
-    syscall(SystemCall::MapMemory, &[env::HEAP_START, env::HEAP_SIZE])
-        .expect("Could not create user heap.");
+    syscall(SystemCall::MapMemory, &[
+            env::HEAP_START, 
+            env::HEAP_SIZE, 
+            MmapFlags::ALLOC_AT.bits() as usize
+        ]).expect("Could not create user heap.");
 
     unsafe {
         ALLOCATOR.lock().init(env::HEAP_START as *mut u8, env::HEAP_SIZE);
