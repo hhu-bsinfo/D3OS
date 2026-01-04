decba8aaa3057fefe182a9a81761dc4cd8495d9f
David Baranwoski <david.baranowski@hhu.de>
2026-01-04 14:18:09 +0100

    more changes

--- os/application/stdlibtest/src/stdlibtest.rs
+++ os/application/stdlibtest/src/stdlibtest.rs
@@ -8,11 +8,13 @@
 //use terminal::println;
 //use std::io::Write;
 //use std::stdio::*;
-use std::io::Write;
-
+//use std::io::prelude::*;
+use std::io::{self, Write};
 
 #[unsafe(no_mangle)]
 pub fn main() {
-    let mut buffer = std::io::stdout();
-    let x = buffer.write(&"12345".as_bytes());
+    let stdout = io::stdout();
+let mut handle = stdout.lock();
+
+handle.write(b"hello world");
 }
