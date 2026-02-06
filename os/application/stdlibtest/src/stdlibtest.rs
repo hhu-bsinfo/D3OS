e7e34eec84e9aa47cb32f179f831d5d457e6471a
David Baranwoski <david.baranowski@hhu.de>
2026-02-06 18:19:58 +0100

    changed print example in stdlibtest, reverted stream and terminal to default, removed #cfg-if comments, removed dependencies from stdlibtest

--- os/application/stdlibtest/src/stdlibtest.rs
+++ os/application/stdlibtest/src/stdlibtest.rs
@@ -3,12 +3,12 @@ use std::io::{self, Write};
 
 #[unsafe(no_mangle)]
 pub fn main() {
-    //writing using stdout directly
+    // writing using stdout directly
     let stdout = io::stdout();
     let mut handle = stdout.lock();
     let _ = handle.write(b"Writing directly with stdout!\n");
 
     print!("This has been written using the print!-macro!\n");
-
-    println!("And this has been written using println!! {}", "wow!");
+    
+    println!("And this has been written using {}!", "println!");
 }
